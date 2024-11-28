fa4bbb77b71bdb232fcccb7b06210877122ef2dc
Chqrety <fikriaverus23@gmail.com>
2024-11-28 23:45:55 +0700

    perf: optimize question

--- src/app/quizz/page.js
+++ src/app/quizz/page.js
@@ -42,12 +42,11 @@ const Quizz = () => {
         //         "Sebuah balok dengan massa 5 kg berada di atas permukaan datar. Koefisien gesek statis antara balok dan permukaan adalah 0,4. Berapakah gaya horizontal minimum yang diperlukan untuk menggerakkan balok tersebut?",
         //     answer: "19,6",
         // },
-        {
-            question:
-                "Sebuah balok bermassa 4 kg ditarik di atas permukaan kasar dengan gaya horizontal sebesar 30 N. Koefisien gesek kinetis antara balok dan permukaan adalah 0,2. Berapakah percepatan balok tersebut?",
-            answer: "5,54",
-        },
-
+        // {
+        //     question:
+        //         "Sebuah balok bermassa 4 kg ditarik di atas permukaan kasar dengan gaya horizontal sebesar 30 N. Koefisien gesek kinetis antara balok dan permukaan adalah 0,2. Berapakah percepatan balok tersebut?",
+        //     answer: "5,54",
+        // },
         // Tambahkan soal lainnya sesuai kebutuhan
     ]
 
@@ -55,7 +54,7 @@ const Quizz = () => {
         const F1 = Math.floor(Math.random() * 99) + 1
         const F2 = Math.floor(Math.random() * 99) + 1
         const answer = F1 + F2
-        const question = `Seorang wanita mendorong sebuah meja dengan gaya F1 = ${F1}N ke arah timur, dan temannya juga mendorong meja dengan gaya F2 = ${F2}N ke arah yang sama. Berapa besar gaya total yang bekerja pada meja tersebut?`
+        const question = `Seorang wanita mendorong sebuah meja dengan gaya F1 = ${F1} N ke arah timur, dan temannya juga mendorong meja dengan gaya F2 = ${F2} N ke arah yang sama. Berapa besar gaya total yang bekerja pada meja tersebut?`
 
         return { question, answer: answer.toString() }
     }
@@ -64,7 +63,7 @@ const Quizz = () => {
         const F1 = Math.floor(Math.random() * 99) + 1
         const F2 = Math.floor(Math.random() * 99) + 1
         const answer = F1 - F2
-        const question = `Rusdi menarik tali dengan gaya F1 = ${F1}N ke arah utara, sementara orang lain menarik tali yang sama dengan gaya F2 = ${F2}N ke arah selatan. Berapa besar resultan gaya yang bekerja?`
+        const question = `Rusdi menarik tali dengan gaya F1 = ${F1} N ke arah utara, sementara orang lain menarik tali yang sama dengan gaya F2 = ${F2} N ke arah selatan. Berapa besar resultan gaya yang bekerja?`
 
         return { question, answer: answer.toString() }
     }
@@ -87,7 +86,7 @@ const Quizz = () => {
         } while (gesek >= total)
         const resultan = total - gesek
         const answer = resultan / massa
-        const question = `Sebuah gaya ${total}N bekerja pada sebuah benda bermassa ${massa}kg. Jika gaya gesekan yang melawan gerak benda sebesar ${gesek}N, berapakah percepatan benda tersebut?`
+        const question = `Sebuah gaya ${total} N bekerja pada sebuah benda bermassa ${massa} kg. Jika gaya gesekan yang melawan gerak benda sebesar ${gesek} N, berapakah percepatan benda tersebut?`
 
         return { question, answer: answer.toString() }
     }
@@ -95,9 +94,10 @@ const Quizz = () => {
     function generateQuestion5() {
         const massa = Math.floor(Math.random() * 99) + 1
         const percepatan = Math.floor(Math.random() * 99) + 1
-        const total = massa * percepatan
+        const total = Math.floor(Math.random() * 99) + 1
+        const resultan = massa * percepatan
         const answer = total - resultan
-        const question = `Sebuah peti bermassa ${massa}kg bergerak di atas lantai kasar dengan percepatan ${percepatan}m/s². Jika gaya total yang bekerja pada peti adalah ${total}N, berapakah gaya gesekan yang dialami peti tersebut?`
+        const question = `Sebuah peti bermassa ${massa} kg bergerak di atas lantai kasar dengan percepatan ${percepatan} m/s². Jika gaya total yang bekerja pada peti adalah ${total} N, berapakah gaya gesekan yang dialami peti tersebut?`
 
         return { question, answer: answer.toString() }
     }
@@ -106,7 +106,7 @@ const Quizz = () => {
         const massa = Math.floor(Math.random() * 99) + 1
         const gaya = Math.floor(Math.random() * 99) + 1
         const answer = massa / gaya
-        const question = `Sebuah balok bermassa ${massa}kg didorong dengan gaya ${gaya}N pada bidang miring licin`
+        const question = `Sebuah balok bermassa ${massa} kg didorong dengan gaya ${gaya} N pada bidang miring licin`
 
         return { question, answer: answer.toString() }
     }
@@ -115,7 +115,7 @@ const Quizz = () => {
         const konstanta = Math.floor(Math.random() * 99) + 1
         const panjang = Math.floor(Math.random() * 99) + 1
         const answer = konstanta * panjang
-        const question = `Sebuah pegas memiliki konstanta pegas k = ${konstanta}N/m . Jika pegas ditarik hingga mengalami pertambahan panjang sebesar ${panjang}m, berapakah besar gaya yang diperlukan untuk menarik pegas tersebut?`
+        const question = `Sebuah pegas memiliki konstanta pegas k = ${konstanta} N/m . Jika pegas ditarik hingga mengalami pertambahan panjang sebesar ${panjang} m, berapakah besar gaya yang diperlukan untuk menarik pegas tersebut?`
 
         return { question, answer: answer.toString() }
     }
@@ -125,7 +125,7 @@ const Quizz = () => {
         const koefisien = Math.random() * 0.9 + 0.1
         const gravitasi = 10
         const answer = massa * koefisien * gravitasi
-        const question = `Sebuah balok dengan massa ${massa}kg berada di atas permukaan datar. Koefisien gesek statis antara balok dan permukaan adalah ${koefisien}. Berapakah gaya horizontal minimum yang diperlukan untuk menggerakkan balok tersebut jika menggunakan gravitasi ${gravitasi}m/s²?`
+        const question = `Sebuah balok dengan massa ${massa} kg berada di atas permukaan datar. Koefisien gesek statis antara balok dan permukaan adalah ${koefisien}. Berapakah gaya horizontal minimum yang diperlukan untuk menggerakkan balok tersebut jika menggunakan gravitasi ${gravitasi} m/s²?`
         return { question, answer: answer.toString() }
     }
 
@@ -136,7 +136,7 @@ const Quizz = () => {
         const gravitasi = 10
         const gesek = koefisien * massa * gravitasi
         const answer = gaya - gesek
-        const question = `Sebuah balok bermassa ${massa}kg ditarik di atas permukaan kasar dengan gaya horizontal sebesar ${gaya}N. Koefisien gesek kinetis antara balok dan permukaan adalah ${koefisien}. Berapakah percepatan balok tersebut?`
+        const question = `Sebuah balok bermassa ${massa} kg ditarik di atas permukaan kasar dengan gaya horizontal sebesar ${gaya} N. Koefisien gesek kinetis antara balok dan permukaan adalah ${koefisien}. Berapakah percepatan balok tersebut?`
         return { question, answer: answer.toString() }
     }
 
